1cc96f084a9de3b002e85787865e481230fa06d4
YingXiaoMing <kenfor-yf@kenfor-yfs-imac.local>
2016-05-30 10:11:20 +0800

    Initial Commit

---
+++ 城市选择Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  城市选择Demo
+//
+//  Created by Kenfor-YF on 16/5/30.
+//  Copyright © 2016年 Kenfor-YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 城市选择Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  城市选择Demo
+//
+//  Created by Kenfor-YF on 16/5/30.
+//  Copyright © 2016年 Kenfor-YF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
